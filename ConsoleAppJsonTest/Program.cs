1a36725e814b06dafc7beb0f924ddd918d93e375
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2020-05-10 22:07:47 +0200

    Add project files.

---
+++ ConsoleAppJsonTest/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ConsoleAppJsonTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Serialize and Deserialize!");
+            Console.WriteLine("Formål: At forstå Json, og hvordan der kan serialiseres og deseriliseres med Json");
+
+            //I dette projekt er der en klasse "Student" som du skal bruge i denne opgave, tag et kig i denne klasse, og læs summary
+
+            //Opgave 1: Installer Newtonsoft Json via Nuget : Tools->NuGet Package Manager  -> Manage ..  for solution
+
+            Console.WriteLine("Serialize C# objekter til Json streng");
+            //Opgave 2: Opret et nyt Student objekt med værdier for navn og adresse, serialiser det med JSonConvert til en variabel, skriv ud til konsollen 
+
+
+            //Opgave 3: Opret en liste List<Student>. Tilføj et antal objekter til listen, serialiser det med JsonConvert til en variabel, skriv ud til konsollen
+
+
+            Console.WriteLine("Deserialiser en json streng til C# objekter");
+
+            //Opgave 4: Brug variablen jsonStudent til at give værdier til et C# objekt (Student), deserialiser det med JSonConvert, print C# objektet ud i konsollen
+            //OBS: bemærk at der er brugt escape karakteren \ foran alle citationstegn \" for at jeg kan have teksten direkte her i koden, disse vil ikke være med i json stregne normalt
+            string jsonStudent = "{\"navn\":\"Mette\",\"adresse\":\"Roskilde\"}";
+
+            //Opgave 5: Brug variablen jsonStudentListe til at give værdier til en liste List<Student>, deserialiser med JsonConvert, foreach over lisen og print hver værdi ud
+            string jsonStudentListe = "[{\"navn\":\"Mette\",\"adresse\":\"Roskilde\"},{\"navn\":\"Anders And\",\"adresse\":\"AndeBy\"},{\"navn\":\"Batman\",\"adresse\":\"Gotham City\"}]";
+
+
+        }
+    }
+}
---
+++ ConsoleAppJsonTest/Student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleAppJsonTest
+{
+    public class Student
+    {
+
+        /// <summary>
+        /// Navn på Studerende
+        /// </summary>
+        public string Navn { get; set; }
+
+        /// <summary>
+        /// Adresse på studerende
+        /// </summary>
+        public string Adresse { get; set; }
+
+        /// <summary>
+        /// Cosntructor som taget to parametre
+        /// </summary>
+        /// <param name="navn">navn på studerende</param>
+        /// <param name="adresse">adresse på studerende</param>
+        public Student(string navn, string adresse)
+        {
+            this.Navn = navn;
+            this.Adresse = adresse;
+        }
+
+        /// <summary>
+        /// default constructor
+        /// </summary>
+        public Student()
+        {
+
+        }
+
+        /// <summary>
+        /// Override af ToString() metoden
+        /// </summary>
+        /// <returns>navn og adresse på den studerende</returns>
+        public override string ToString()
+        {
+            return $"Navn: {Navn} , Adresse: {Adresse}";
+        }
+    }
+}
